9286bc857edd5ce63b28ded2bd0e9ddbd21843f7
mariagarciadelarosa92-star <mariagarciadelarosa92@gmail.com>
2026-09-20 01:41:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lvn-es-clean-v1';
+const CACHE='lvn-es-clean-v2';
 const ASSETS=['./manifest.json'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
